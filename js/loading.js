0bd0a1b80e59c21e67a8d5798cd923de0b833158
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-02-27 22:47:00 +0000

    Tue, Feb 27, 2024, 2:47 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -3,11 +3,15 @@ bg = document.querySelector('.bg')
 
 load = 0
 
-blurring()
+int = setInterval(blurring, 35)
 
 
 function blurring() {
     load++
+
+    if (load > 99){
+        clearInterval(int)
+    }
     loadText.innerText = `${load}%`
     loadText.style.opacity = 1
     bg.style.filter = "blur(10px)"
