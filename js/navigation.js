6a94b18e7cdc1ab3f148109fd2e93bbe2371a1fc
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-03-07 21:54:37 +0000

    Thu, Mar 7, 2024, 1:54 PM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', fixNav)
 
 function fixNav(){
 
-    if (window.scrollY > 1400) {
+    if (window.scrollY > 256) {
         nav.classList.add('active')
     } else{
         nav.classList.remove('active')
