303f05fb2a46e8014721f8054d272d64e8dbaba4
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-03-12 03:10:54 +0000

    Mon, Mar 11, 2024, 8:10 PM -07:00

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ button=document.querySelector('.btn')
 
 
 load = 0
-
+size = 0
 button.addEventListener('click', () =>{
     int = setInterval(blurring, 35)
 
@@ -13,13 +13,14 @@ button.addEventListener('click', () =>{
 
 function blurring() {
     load++
+    size++
     if (load > 99){
         clearInterval(int)
     }
 
     loadText.style.opacity = 0 + load/100
-    loadText.style.width = 0 + load
-    loadText.style.height = 0 + load
+    loadText.style.width = 0 + size
+    loadText.style.height = 0 + size
 
 
 }
