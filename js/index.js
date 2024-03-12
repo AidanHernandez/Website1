5497ce5a86cfcb22204b33701415681aea2c1cd7
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-03-12 21:45:47 +0000

    Tue, Mar 12, 2024, 2:45 PM -07:00

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ function smol() {
     smaller++
     smaller++
     if (smaller > 99){
-        document.body.style.backgroundImage= ""
+        document.body.style.backgroundImage= "back-images-in-js/white.png"
         clearInterval(int)
 
     }
