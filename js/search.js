936d5ec86c92a8171d86f3351507e707c95ba82a
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-03-12 02:52:42 +0000

    Mon, Mar 11, 2024, 7:52 PM -07:00

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 btn = document.querySelector('.btn')
 search = document.querySelector('.search')
-
+console.log("hrllo")
 btn.addEventListener('click', () => {
     search.classlist.add('active')
 })
