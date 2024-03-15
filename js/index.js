a284bf9d28def990c059b8d1a5339dc1ddd79377
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2024-03-15 21:40:14 +0000

    Fri, Mar 15, 2024, 2:40 PM -07:00

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ overlay = document.querySelector('.container')
 body = document.querySelector('body')
 links = document.querySelector('a')
 main_title = document.querySelector('.maintitle')
+nav = document.querySelector('.nav')
 
 load = 0
 smaller = 0;
@@ -51,6 +52,7 @@ function smol() {
     smaller++
     smaller++
     if (smaller > 99){
+        nav.style.opacity = 0 + smaller/100
         maintitle.style.zIndex = 0;
         links.style.zIndex = 0;
         body.style.overflow = "visible";
